26fcfb47de1e2d7e6364498c752f3ae802451d53
Gizeta <0w0@gizeta.tk>
2015-04-07 01:30:56 +0900

    改进缓存清除的代码实现。

--- KanColleModifier.KCV/ModifierViewModel.cs
+++ KanColleModifier.KCV/ModifierViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Fiddler;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -18,11 +19,43 @@ namespace Gizeta.KanColleModifier.KCV
         private string ipAddress = "";
         private string jsonData = "";
 
+        #region Win32
+
+        public struct INTERNET_CACHE_ENTRY_INFO
+        {
+            public int dwStructSize;
+            public IntPtr lpszSourceUrlName;
+            public IntPtr lpszLocalFileName;
+            public int CacheEntryType;
+            public int dwUseCount;
+            public int dwHitRate;
+            public int dwSizeLow;
+            public int dwSizeHigh;
+            public FILETIME LastModifiedTime;
+            public FILETIME ExpireTime;
+            public FILETIME LastAccessTime;
+            public FILETIME LastSyncTime;
+            public IntPtr lpHeaderInfo;
+            public int dwHeaderInfoSize;
+            public IntPtr lpszFileExtension;
+            public int dwExemptDelta;
+        }
+
+        const int ERROR_NO_MORE_ITEMS = 259;
+
+        [DllImport(@"wininet.dll", SetLastError = true)]
+        public static extern IntPtr FindFirstUrlCacheEntry(string lpszUrlSearchPattern, IntPtr lpFirstCacheEntryInfo, ref int lpdwFirstCacheEntryInfoBufferSize);
+        [DllImport(@"wininet.dll", SetLastError = true)]
+        public static extern bool FindNextUrlCacheEntry(IntPtr hEnumHandle, IntPtr lpNextCacheEntryInfo, ref int lpdwNextCacheEntryInfoBufferSize);
+        [DllImport(@"wininet.dll", SetLastError = true)]
+        public static extern bool FindCloseUrlCache(IntPtr hEnumHandle);
         [DllImport(@"wininet.dll", SetLastError = true)]
         public static extern long DeleteUrlCacheEntry(string lpszUrlName);
 
         [DllImport("kernel32", EntryPoint = "GetPrivateProfileStringW", CharSet = CharSet.Unicode)]
         private static extern int GetPrivateProfileString(string section, string key, string defVal, byte[] retVal, int size, string filePath);
+
+        #endregion
 
         private ModifierViewModel()
         {
@@ -103,7 +136,7 @@ namespace Gizeta.KanColleModifier.KCV
 
         public ICommand CleanCache
         {
-            get { return new RelayCommand(() => list.ForEach(x => x.CleanIECache(ipAddress))); }
+            get { return new RelayCommand(() => clearIECache()); }
         }
         
         #region private method
@@ -248,8 +281,6 @@ namespace Gizeta.KanColleModifier.KCV
             string sortNo = Regex.Match(graphStr, @"api_sortno"":(\d+)").Groups[1].Value;
             string infoStr = Regex.Match(jsonData, @"\{([^{]+?)api_sortno"":" + sortNo + @"([^}]+?)\}").Groups[0].Value;
 
-            info.GraphVersion = Regex.Match(graphStr, @"api_version"":""(.+?)""").Groups[1].Value;
-
             if (info.ResourceIniPath == null) return;
             string graphReplaceStr = graphStr;
             string infoReplaceStr = infoStr;
@@ -292,6 +323,51 @@ namespace Gizeta.KanColleModifier.KCV
             return Encoding.Unicode.GetString(sb, 0, buf * 2).Trim();
         }
 
+        private void clearIECache()
+        {
+            int nNeeded = 0, nBufSize;
+            IntPtr buf;
+            INTERNET_CACHE_ENTRY_INFO CacheItem;
+            IntPtr hEnum;
+            bool r;
+
+            FindFirstUrlCacheEntry(null, IntPtr.Zero, ref nNeeded);
+
+            if (Marshal.GetLastWin32Error() == ERROR_NO_MORE_ITEMS)
+                return;
+
+            nBufSize = nNeeded;
+            buf = Marshal.AllocHGlobal(nBufSize);
+            hEnum = FindFirstUrlCacheEntry(null, buf, ref nNeeded);
+            while (true)
+            {
+                CacheItem = (INTERNET_CACHE_ENTRY_INFO)Marshal.PtrToStructure(buf, typeof(INTERNET_CACHE_ENTRY_INFO));
+                string url = Marshal.PtrToStringAnsi(CacheItem.lpszSourceUrlName);
+                if (url.IndexOf(ipAddress) >= 0 && (url.IndexOf("/kcs/resources/swf/ships/") >= 0 || url.IndexOf("/kcs/resources/swf/font.swf") >= 0))
+                {
+                    if (list.Any(x => x.HitTest(url)))
+                    {
+                        DeleteUrlCacheEntry(url);
+                    }
+                }
+
+                nNeeded = nBufSize;
+                r = FindNextUrlCacheEntry(hEnum, buf, ref nNeeded);
+
+                if (!r && Marshal.GetLastWin32Error() == ERROR_NO_MORE_ITEMS)
+                    break;
+
+                if(!r && nNeeded > nBufSize)
+                {
+                    nBufSize = nNeeded;
+                    buf = Marshal.ReAllocHGlobal(buf, (IntPtr)nBufSize);
+                    FindNextUrlCacheEntry(hEnum, buf, ref nNeeded);
+                }
+            }
+
+            Marshal.FreeHGlobal(buf);
+        }
+
         #region Fiddler
 
         private void setModifier()
@@ -327,10 +403,6 @@ namespace Gizeta.KanColleModifier.KCV
             if (ModifierOn && (oSession.fullUrl.IndexOf("/kcs/resources/swf/ships/") >= 0 || oSession.fullUrl.IndexOf("/kcs/resources/swf/font.swf") >= 0))
             {
                 var info = list.First(x => x.HitTest(oSession.fullUrl));
-                if (info.GraphKey == "font")
-                {
-                    info.GraphVersion = Regex.Match(oSession.fullUrl, @"version=([\d.]+)").Groups[1].Value;
-                }
                 if (info != null)
                     info.ReplaceResponseBody(oSession);
             }
@@ -358,12 +430,12 @@ namespace Gizeta.KanColleModifier.KCV
         internal class ModifyInfo
         {
             public string GraphKey { get; set; }
-            public string GraphVersion { get; set; }
             public string ResourcePath { get; set; }
             public string ResourceIniPath { get; set; }
 
             public bool HitTest(string url)
             {
+                if (ResourcePath == null) return false;
                 var tmp1 = url.Split('/');
                 var tmp2 = tmp1.Last().Split('.');
                 if (tmp2.Length >= 1)
@@ -380,19 +452,6 @@ namespace Gizeta.KanColleModifier.KCV
                 session.oResponse.headers.HTTPResponseCode = 200;
                 session.oResponse.headers.HTTPResponseStatus = "200 OK";
                 session.oResponse.headers["Content-Type"] = "application/x-shockwave-flash";
-            }
-
-            public void CleanIECache(string ipAddress)
-            {
-                if (ipAddress == "" || ResourcePath == null) return;
-                if (this.GraphKey == "font")
-                {
-                    DeleteUrlCacheEntry(string.Format("http://{0}/kcs/resources/swf/font.swf?version={1}", ipAddress, this.GraphVersion));
-                }
-                else
-                {
-                    DeleteUrlCacheEntry(string.Format("http://{0}/kcs/resources/swf/ships/{1}.swf?VERSION={2}", ipAddress, this.GraphKey, this.GraphVersion));
-                }
             }
         }
     }
